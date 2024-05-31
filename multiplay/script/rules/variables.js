14b9bafc914b910bcff196364aefb198b9d94a27
KJeff01 <kjeff1997@gmail.com>
2024-05-30 20:27:55 -0500

    Pick new Tech Level times
    Probably bad, just needs something that at least seems more reasonable for this balance.

--- multiplay/script/rules/variables.js
+++ multiplay/script/rules/variables.js
@@ -12,7 +12,7 @@ const DESTROY_LIKE_EVENT = 1;
 const TRANSFER_LIKE_EVENT = 2;
 
 const cleanTech = 1;
-const timeBaseTech = 3*60;		// after Half-tracked Propulsion and Light Cannon
-const timeAdvancedBaseTech = 6.4*60;	// after Factory Module and HEAT Cannon Shells Mk2
-const timeT2 = 17*60;
-const timeT3 = 26*60;			// after Needle Gun and Scourge Missile
+const timeBaseTech = 259;		// after Half-tracked Propulsion and Mini-Rocket Pod
+const timeAdvancedBaseTech = 480;	// after Mortar
+const timeT2 = 1098;            // after Assault Gun
+const timeT3 = 1788;			// after Needle Gun and Scourge Missile
